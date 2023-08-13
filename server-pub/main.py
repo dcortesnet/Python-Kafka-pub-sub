042c97ea82dc8c2bd63a99e711b92d3e513f5eed
dcortesnet <dcortes.net@gmail.com>
2023-08-13 11:46:13 -0400

    Server pub and sub connectors

--- server-pub/main.py
+++ server-pub/main.py
@@ -1,26 +1,30 @@
 from fastapi import FastAPI, Request, HTTPException
 import json
+import uvicorn
 from datetime import datetime
 import uuid
+from kafka import KafkaProducer
 
 app = FastAPI()
+name_topic = "message_topic"
+kafka_producer = KafkaProducer(bootstrap_servers="localhost:9092")
 
 @app.post("/messages")
-async def publish_message(request: Request):
+async def messages(request: Request):
     try:
         body = (await request.body()).decode('utf-8')
         data = json.loads(body)
         message = {
-            "id": uuid.uuid4(),
+            "id": str(uuid.uuid4()),
             "message": data['message'],
-            "date": datetime.now()
-        }        
-        
+            "date": str(datetime.now())
+        }
+        kafka_producer.send(name_topic, value=json.dumps(message).encode('utf-8'))
         print(f"Publishing an Event using Kafka to: {data['message']}");
         return { "detail": "Publishing an Event using Kafka successful" }
     except json.JSONDecodeError:
         raise HTTPException(status_code=400, detail="Invalid request body")
-    except:
+    except Exception as e:
         raise HTTPException(status_code=500, detail="Internal Server error")
     
 if __name__ == "__main__":
--- server-sub/main.py
+++ server-sub/main.py
@@ -1,11 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
+from kafka import KafkaConsumer
+import json
+import threading
 
 app = FastAPI()
+name_topic = "message_topic"
 
-@app.get("/")
-def index():
-    return {"Hello": "World"}
+messages_storage = []
 
+def kafka_consumer_thread():
+    consumer = KafkaConsumer(
+        name_topic,
+        group_id='my-group',
+        bootstrap_servers="localhost:9092",
+        value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+    )
+    for message in consumer:
+        json_message = message.value       
+        print("Received JSON message:", json_message)
+        messages_storage.append(json_message)
+
+kafka_thread = threading.Thread(target=kafka_consumer_thread)
+kafka_thread.daemon = True
+kafka_thread.start()
+
+@app.get("/messages")
+def messages():
+    try:
+        return { "messages": messages_storage }    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server error")
+    
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)
